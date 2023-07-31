e2e03bc2587b735a8d9d409b1a1c20175be09e2a
Danilo Correia <danilocorreiaviana@hotmail.com>
2023-07-31 16:00:07 -0300

    Listagem de moedas - componentes, comunicação API e métodos

---
+++ src/app/coin-list/Icoin-list.ts
@@ -0,0 +1,4 @@
+export interface Coin {
+    code: string;
+    description: string;
+}
--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,10 +1,107 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Coin } from './Icoin-list';
+import { map, catchError, finalize } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-coin-list',
   templateUrl: './coin-list.component.html',
   styleUrls: ['./coin-list.component.css']
 })
-export class CoinListComponent {
-  // Código do componente
+export class CoinListComponent implements OnInit {
+  apiUrl = 'https://api.exchangerate.host/symbols';
+  coinList: Coin[] = [];
+  coinListCompleta: Coin[] = [];
+  totalMoedas: number = 0;
+  paginaAtual = 1;
+  itensPorPagina = 5
+  busca: string = '';
+  campoOrdenacao: string = 'code';
+  filtroOrdenacao: string = 'symbolAsc'
+  loading = true;
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.carregarCoinList();
+  }
+
+  carregarCoinList() {
+    this.http.get<any>(this.apiUrl).pipe(
+      map((response) => {
+        this.coinListCompleta = Object.keys(response.symbols).map((key) => ({
+          code: key,
+          description: response.symbols[key].description,
+        }));
+        this.coinList = this.coinListCompleta.slice(); // Cópia da lista completa
+        this.totalMoedas = this.coinList.length; // Definir o total de moedas
+      }),
+      catchError((error) => {
+        return throwError(() => error);
+      }),
+      finalize(() => {
+        this.loading = false;
+      })
+    ).subscribe();
+  }
+
+  buscarCoins() {
+    const valorBusca = this.busca.trim().toLowerCase();
+
+    if (!valorBusca) {
+      this.coinList = this.coinListCompleta.slice(); // Restaurar a lista completa
+      this.totalMoedas = this.coinListCompleta.length; // Atualizar o total de moedas
+      this.ordenarPor(this.filtroOrdenacao);
+    } else {
+      this.coinList = this.coinListCompleta.filter(coin =>
+        this.verificarTexto(coin.code, valorBusca) ||
+        this.verificarTexto(coin.description, valorBusca)
+      );
+      this.totalMoedas = this.coinList.length; // Atualizar o total de moedas após a busca
+    }
+
+    // Verificar se a página atual está fora dos limites após a busca
+    if (this.paginaAtual > Math.ceil(this.totalMoedas / this.itensPorPagina)) {
+      this.paginaAtual = Math.max(1, Math.ceil(this.totalMoedas / this.itensPorPagina));
+    }
+  }
+
+  private verificarTexto(texto: string, valorBusca: string): boolean {
+    return texto.toLowerCase().includes(valorBusca);
+  }
+
+  limparBusca() {
+    this.busca = '';
+    this.coinList = this.coinListCompleta.slice();
+    this.totalMoedas = this.coinListCompleta.length;
+    this.ordenarPor(this.filtroOrdenacao);
+  }
+
+  ordenarPor(opcao: string) {
+    this.filtroOrdenacao = opcao;
+    switch (opcao) {
+      case 'symbolAsc':
+        this.coinList.sort((a, b) => a.code.localeCompare(b.code));
+        break;
+      case 'symbolDesc':
+        this.coinList.sort((a, b) => b.code.localeCompare(a.code));
+        break;
+      case 'descriptionAsc':
+        this.coinList.sort((a, b) => a.description.localeCompare(b.description));
+        break;
+      case 'descriptionDesc':
+        this.coinList.sort((a, b) => b.description.localeCompare(a.description));
+        break;
+      default:
+        break;
+    }
+  }
+
+  atualizarPaginacao(pagina: number) {
+    this.paginaAtual = pagina;
+    if (!this.itensPorPagina) {
+      this.itensPorPagina = 5;
+    }
+  }
 }
