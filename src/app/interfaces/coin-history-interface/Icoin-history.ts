b0c62b0018f3533beb17699a60621e0c48c3ce43
Danilo Correia <danilocorreiaviana@hotmail.com>
2023-08-14 12:01:12 -0300

    Criação de interface de histórico'

---
+++ src/app/interfaces/coin-history-interface/Icoin-history.ts
@@ -0,0 +1,9 @@
+export default interface HistoryInterface {
+    date: string;
+    time: string;
+    input: number;
+    output: number;
+    originCurrency: string;
+    destinyCurrency: string;
+    rate: number;
+  }
