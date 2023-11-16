2ca076681148f161bbffd8153f078bbb29ad56e8
Danilo Correia <danilocorreiaviana@hotmail.com>
2023-11-16 12:28:35 -0300

    refactor: Tratamento de rota inexistente

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { HomeComponent } from './home/home.component';
 import { CoinListComponent } from './coin-list/coin-list.component';
 import { CurrencyConversionComponent } from './currency-conversion/currency-conversion.component';
 import { CoinHistoryComponent } from './coin-history/coin-history.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'coin-list', component: CoinListComponent },
   { path: 'currency-conversion', component: CurrencyConversionComponent },
   { path: 'coin-history', component: CoinHistoryComponent },
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: '/page-not-found' }
 ];
 
 @NgModule({
